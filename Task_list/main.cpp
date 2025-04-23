fe94f5f142a6aa5be9bd0f31dbd6c4700030d332
Joe Fuller <Joseph.tgfuller@gmail.com>
2025-04-23 06:01:35 -0400

    Initial Commit

---
+++ Task_list/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Task_list
+//
+//  Created by Joe Fuller on 4/23/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
